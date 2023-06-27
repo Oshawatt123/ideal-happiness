e9e2c842c6cd93712dbb70e8dc701551473b7bf2
Lewis <lewis.arnold@hotmail.co.uk>
2023-06-27 10:15:52 +0100

    Collision Detection (broken)

--- Relativity/Assets/Relativity/Particle.cs
+++ Relativity/Assets/Relativity/Particle.cs
@@ -5,8 +5,28 @@ using UnityEngine;
 public class Particle : MonoBehaviour
 {
     // Debug
+    enum GizmoType
+    {
+        Line,
+        Sphere
+    }
+    class GizmoDrawOptions
+    {
+        public GizmoType gizmoType;
+
+        // Line
+        public Vector3 forceLine;
+
+        // Sphere
+        public Vector3 position;
+        public float radius;
+
+        public Color color;
+
+        public float time = 0f;
+    }
     public bool DrawDebugGizmos = false;
-    private List<KeyValuePair<Vector3, Color>> GizmosQueue = new List<KeyValuePair<Vector3, Color>>();
+    private List<GizmoDrawOptions> GizmosQueue = new List<GizmoDrawOptions>();
 
     // Globals
     public /*const*/ Vector3 GRAVITY = new Vector3(0f, -9.8f, 0f);        // Gravity
@@ -14,6 +34,7 @@ public class Particle : MonoBehaviour
     public const float COEFFICIENTOFDRAG = 0.47f; // measured result https://en.wikipedia.org/wiki/Drag_coefficient
     public const float WINDSPEED = 20.0f;                                // idk its whatever we choose, its wind speed
     public /*const*/ Vector3 WINDDIRECTION = new Vector3(1f, 0f, 0f);   // ^^  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~  ^^ direction
+    public /*const*/ float GROUND_Y_POSITION = 0f;
 
 
     public float Mass = 1.0f;                                   // Mass
@@ -23,6 +44,13 @@ public class Particle : MonoBehaviour
     public Vector3 Forces = Vector3.zero;                       // Total force acting on this particle
     public float Radius = 0.1f;                                 // Radius of the particle
 
+    // Collision
+    private Vector3 PreviousPosition;
+    private Vector3 ImpactForces;
+    private bool HasCollided;
+    [SerializeField]
+    private float CoefficientOfRestitution = 0.6f;
+
     // Constructor
     private void Start()
     {
@@ -31,15 +59,104 @@ public class Particle : MonoBehaviour
 
     private void Update()
     {
+        float timeStep = 0.5f;
         // Debug
-        if(DrawDebugGizmos)
+        if (DrawDebugGizmos)
         {
-            GizmosQueue.Clear();
+            List<GizmoDrawOptions> gizmosToClear = new List<GizmoDrawOptions>();
+            GizmosQueue.ForEach(drawOption =>
+            {
+                if(drawOption.time != -1) // time of -1 means persistent, skip removal
+                {
+                    if(drawOption.time == 0) // time of 0 means one frame, so force removal
+                    {
+                        gizmosToClear.Add(drawOption);
+                    }
+                    else
+                    {
+                        drawOption.time -= Time.deltaTime;
+                        if (drawOption.time < 0)
+                        {
+                            gizmosToClear.Add(drawOption);
+                        }
+                    }
+                }
+            });
+
+            gizmosToClear.ForEach(drawOption =>
+            {
+                GizmosQueue.Remove(drawOption);
+            });
         }
+
+        HasCollided = CheckForCollisions(timeStep);
 
         CalcLoads();
 
-        UpdateBodyEuler(Time.deltaTime);
+        PreviousPosition = transform.position;
+        UpdateBodyEuler(timeStep);
+
+    }
+
+    public bool CheckForCollisions(float dt)
+    {
+        // Setup
+        Vector3 collisionNormal = Vector3.zero;
+        Vector3 relativeVelocity = Vector3.zero;
+        float relativeVelocityDot = 0f;
+        float collisionImpulse = 0f;
+        Vector3 collisionImpactForce = Vector3.zero;
+        bool hasCollided = false;
+
+        ImpactForces = Vector3.zero;
+
+        // Collision Detection
+
+        // Ground Check
+        if(transform.position.y <= GROUND_Y_POSITION)
+        {
+            collisionNormal.x = 0;
+            collisionNormal.y = 1;          // ground is static, a flat plane, and we are only interested in collisions from above; we can determine our collision normal as being Y up
+            relativeVelocity = Velocity;    // ground is static, so relative velocity i just the velocity of the particle
+            relativeVelocityDot = Vector3.Dot(relativeVelocity, collisionNormal); // dot product determines how parallel our vector are
+
+            // if dot is negative, aka vectors pointing away from each other
+            // aka velocity is opposite to collision normal
+            // aka we are moving towards the ground
+            if(relativeVelocityDot < 0f)
+            {
+                collisionImpulse = -(relativeVelocityDot) * (CoefficientOfRestitution + 1) * Mass;
+                collisionImpactForce = collisionNormal;
+                collisionImpactForce.Scale(collisionImpactForce / dt); // vector3.scale is vector multiplication
+
+                ImpactForces += collisionImpactForce;
+
+                // reposition particle so that it collides with the ground rather than through it
+                Vector3 newPosition = Vector3.zero;
+                newPosition.z = transform.position.z;               // same as before
+                newPosition.y = GROUND_Y_POSITION + Radius;         // Y position, plus radius of our particle
+
+                // calculating our X position is slightly more difficult.
+                // we need to calculate the percentage of the timestep moved in the Y and use that to calculate our new X
+                float percentageOfTimestepMoved = (newPosition.y - PreviousPosition.y) / (newPosition.y - transform.position.y);
+
+                float calculatedX = (transform.position.y - PreviousPosition.y) * percentageOfTimestepMoved;
+                newPosition.x = calculatedX;
+
+                GizmoDrawOptions newPosDrawOptions = new GizmoDrawOptions();
+                newPosDrawOptions.gizmoType = GizmoType.Sphere;
+                newPosDrawOptions.position = newPosition;
+                newPosDrawOptions.color = Color.yellow;
+                newPosDrawOptions.time = -1;
+                AddGizmoToDraw(newPosDrawOptions);
+
+                transform.position = newPosition;
+
+                return true;
+            }
+        }
+
+        return false;
     }
 
     // Aggregate forces acting on this particle
@@ -50,9 +167,19 @@ public class Particle : MonoBehaviour
 
         // Aggregate Forces
 
+        // Collision
+        if(HasCollided)
+        {
+            AddForceInternal(ImpactForces);
+            return; // early return on frames when we collide
+        }
+
         // Gravity
-        Gizmos.color = Color.yellow;
-        AddForceInternal(GRAVITY, true);
+        GizmoDrawOptions GravityDrawOptions = new GizmoDrawOptions();
+        GravityDrawOptions.color = Color.yellow;
+        GravityDrawOptions.gizmoType = GizmoType.Line;
+        GravityDrawOptions.forceLine = GRAVITY;
+        AddForceInternal(GRAVITY, GravityDrawOptions);
 
 #region Still Air Drag
         // Still Air Drag
@@ -83,8 +210,11 @@ public class Particle : MonoBehaviour
         AirDragForce *= AirDragForceMag;
 
         // Apply Still Air Drag
-        Gizmos.color = Color.blue;
-        AddForceInternal(AirDragForce, true);
+        GizmoDrawOptions AirDragDrawOptions = new GizmoDrawOptions();
+        AirDragDrawOptions.color = Color.blue;
+        AirDragDrawOptions.gizmoType = GizmoType.Line;
+        AirDragDrawOptions.forceLine = AirDragForce;
+        AddForceInternal(AirDragForce, AirDragDrawOptions);
 #endregion
 
 #region Wind
@@ -102,19 +232,30 @@ public class Particle : MonoBehaviour
         WindForce *= WindForceMag;
 
         // Apply Still Air Drag
-        Gizmos.color = Color.red;
-        AddForceInternal(WindForce, true);
+        GizmoDrawOptions WindDrawOptions = new GizmoDrawOptions();
+        WindDrawOptions.color = Color.red;
+        WindDrawOptions.gizmoType = GizmoType.Line;
+        WindDrawOptions.forceLine = WindForce;
+        AddForceInternal(WindForce, WindDrawOptions);
 #endregion
 
     }
 
-    private void AddForceInternal(Vector3 force, bool gizmoColorSet = false)
+    private void AddForceInternal(Vector3 force, GizmoDrawOptions drawOptions = null)
     {
-        if(DrawDebugGizmos && gizmoColorSet)
+        if(DrawDebugGizmos && drawOptions != null)
         {
-            GizmosQueue.Add(new KeyValuePair<Vector3, Color>(force, Gizmos.color));
+            GizmosQueue.Add(drawOptions);
         }
         Forces += force;
+    }
+
+    private void AddGizmoToDraw(GizmoDrawOptions drawOptions)
+    {
+        if(DrawDebugGizmos)
+        {
+            GizmosQueue.Add(drawOptions);
+        }
     }
 
     // Integrate one time step dt
@@ -144,10 +285,17 @@ public class Particle : MonoBehaviour
     {
         if(DrawDebugGizmos)
         {
-            GizmosQueue.ForEach(pair =>
+            GizmosQueue.ForEach(drawOption =>
             {
-                Gizmos.color = pair.Value;
-                Gizmos.DrawLine(transform.position, transform.position + pair.Key);
+                Gizmos.color = drawOption.color;
+                if(drawOption.gizmoType == GizmoType.Line)
+                {
+                    Gizmos.DrawLine(transform.position, transform.position + drawOption.forceLine);
+                }
+                else if(drawOption.gizmoType == GizmoType.Sphere)
+                {
+                    Gizmos.DrawWireSphere(drawOption.position, drawOption.radius);
+                }
             });
         }
     }
